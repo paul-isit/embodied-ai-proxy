258cd7989baf6305998ac98403b24bb875bfae0d
paul-isit <paulitis00@gmail.com>
2026-04-29 10:16:02 +0000

    just some comments

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.frontend.tui_app import EmbodiedProxyApp
 from src.backend.llm_proxy import LLMProxy
 
 def main():
+    # add logic here to pass in the path to the configs/ directory
+    # and initialize the LLMProxy class like this:
+    # proxy = LLMProxy(path_to_configs_directory)
     proxy = LLMProxy()
     app = EmbodiedProxyApp(proxy)
     app.run()
